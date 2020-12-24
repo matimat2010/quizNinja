d2a9179923019baf8bac60584d303b406e664cfa
Matt Alvarado <matimat2010@gmail.com>
2020-12-23 18:28:04 -0600

    Book completed

--- chapter/animation/main.js
+++
@@ -1,9 +0,0 @@
-const squareElement = document.getElementById('square');
-let angle = 0;
-
-setInterval( () => {
-
-    angle = (angle + 2) % 360;
-    squareElement.style.transform = `rotate(${angle}deg)`
-    }, 1000/60);
-    
